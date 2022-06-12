2a362dc9a72615cdd87851b59cfdcfd6f5ea8362
Johann Vivas <yovivstolkan@gmail.com>
2022-06-12 23:32:25 +0800

    added damage block trigger collider to simulate a fire trap, and feedback logs to show iframes are working.

--- DSFiles/Assets/Scripts/PlayerManager.cs
+++ DSFiles/Assets/Scripts/PlayerManager.cs
@@ -8,6 +8,8 @@ namespace agahan_vivas
     {
         InputHandler inputHandler;
         Animator anim;
+        CapsuleCollider capsuleCollider;
+        SphereCollider sphereCollider;
 
         public bool isInvulnerable;
 
@@ -15,6 +17,8 @@ namespace agahan_vivas
         {
             inputHandler = GetComponent<InputHandler>();    
             anim = GetComponentInChildren<Animator>();
+            capsuleCollider = GetComponentInChildren<CapsuleCollider>();
+            sphereCollider = GetComponentInChildren<SphereCollider>();
         }
 
         void Update()
@@ -26,5 +30,38 @@ namespace agahan_vivas
             //i don't know if this actually does anything aside from showing na invulnerable yung character at the current frame
             isInvulnerable = anim.GetBool("isInvulnerable");
         }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.isTrigger)
+            {
+                if (!isInvulnerable)
+                {
+                    Debug.Log("Ouch! damage taken.");
+                }
+                else
+                {
+                    Debug.Log("Damage avoided!");
+                }
+            }
+        }
+
+        /*
+        private void OnCollisionEnter(Collision collision)
+        {
+            foreach (ContactPoint contact in collision.contacts)
+            {
+                print(contact.otherCollider.isTrigger);
+                if (!isInvulnerable && contact.otherCollider.isTrigger && contact.thisCollider == capsuleCollider)
+                {
+                    Debug.Log("OUCH! That hurts!");
+                }
+                else
+                {
+                    //Debug.Log("Either invulnerable or not working right.");
+                }
+            }
+        }
+        */
     }
 }
